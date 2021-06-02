8f00ae9247a061c4ae876c12f6200db7757e0247
Avantgarde95 <mathematicianscott@gmail.com>
2021-06-02 11:17:33 +0900

    [feat] Project setup
    - Create the configuration files for compiling .ts and .scss.
    - Basic webpage for testing

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const App = () => (
+    <div className={'App'}>
+        Hello!
+    </div>
+);
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function getAbsolutePath(...paths) {
+    return path.resolve(__dirname, ...paths);
+}
+
+module.exports = (env, argv) => {
+    const isDebugMode = !argv || argv.mode === 'development';
+    const srcPath = 'src';
+    const outPath = 'docs';
+
+    return {
+        mode: 'development',
+        entry: getAbsolutePath(srcPath, 'Main.tsx'),
+        devtool: isDebugMode ? 'inline-source-map' : false,
+        target: ['web', 'es3'],
+        output: {
+            path: getAbsolutePath(outPath),
+            filename: '[name].[contenthash].js'
+        },
+        optimization: {
+            splitChunks: {
+                chunks: 'all'
+            }
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+                        }
+                    }
+                },
+                {
+                    test: /\.(css|scss)$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css'
+            }),
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: getAbsolutePath(srcPath, 'Template.html'),
+                filename: getAbsolutePath(outPath, 'index.html')
+            })
+        ]
+    };
+};
