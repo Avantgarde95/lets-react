ab465b493585c6621beebfcd02d3bfc14e0b2d36
Avantgarde95 <95phm@kaist.ac.kr>
2021-06-05 15:43:33 +0900

    [bugfix] Add missing syntax highlighting
    * Register JSON to CodeView

--- src/view/viewer/ContentView.tsx
+++ src/view/viewer/ContentView.tsx
@@ -4,12 +4,14 @@ import { Components } from 'react-markdown/src/ast-to-react';
 import { PrismLight as CodeView } from 'react-syntax-highlighter';
 import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
 import tsx from 'react-syntax-highlighter/dist/esm/languages/prism/tsx';
+import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json';
 import xonokai from 'react-syntax-highlighter/dist/esm/styles/prism/xonokai';
 
 CodeView.registerLanguage('jsx', jsx);
 CodeView.registerLanguage('javascript', jsx);
 CodeView.registerLanguage('tsx', tsx);
 CodeView.registerLanguage('typescript', tsx);
+CodeView.registerLanguage('json', json);
 
 const components: Components = {
     code({ inline, className, children }) {
