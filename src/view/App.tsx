ba3a4527122cf5ac7078787899e5072249e1c089
Avantgarde95 <95phm@kaist.ac.kr>
2021-06-06 17:07:26 +0900

    [feat] Add some buttons on the header
    * 'Menu' button
      * For opening/closing the explorer (Not implemented yet)
    * 'Code' button (actually a link)
      * Open the repository of this website

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -4,7 +4,16 @@ import { Viewer } from 'view/viewer/Viewer';
 
 const Header = () => (
     <div className={'Header'}>
-        Let's react
+        <button className={'Button'}>Menu</button>
+        <span className={'Title'}>Let's react</span>
+        <a
+            className={'Button'}
+            href={'https://github.com/Avantgarde95/lets-react'}
+            target={'_blank'}
+            rel={'noopenner noreferrer'}
+        >
+            Code
+        </a>
     </div>
 );
 
