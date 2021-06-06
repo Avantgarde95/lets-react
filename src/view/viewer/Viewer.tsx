0992b024bbb9fbf184787e4ac9f77fdb5b8fffa5
Avantgarde95 <95phm@kaist.ac.kr>
2021-06-06 17:44:22 +0900

    [feat] Click the viewer to close the menu
    * The user can close the menu by clicking the viewer, too.
      * It makes closing the menu more convenient in the mobile device.

--- src/view/viewer/Viewer.tsx
+++ src/view/viewer/Viewer.tsx
@@ -1,13 +1,20 @@
 import React, { useContext } from 'react';
 import { ArticleContext } from 'store/ArticleContext';
+import { ViewContext } from 'store/ViewContext';
 import { SectionView } from 'view/viewer/SectionView';
 
 export const Viewer = () => {
     const { articles, articleIndex } = useContext(ArticleContext);
+    const { openMenu } = useContext(ViewContext);
     const article = articles[articleIndex];
 
     return (
-        <div className={'Viewer'}>
+        <div
+            className={'Viewer'}
+            onClick={() => {
+                openMenu(false);
+            }}
+        >
             <div className={'ArticleTitle'}>{article.title}</div>
             {article.sections.map((section, index) => (
                 <SectionView index={index} section={section} />
