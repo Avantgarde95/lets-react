811941e5ad57a27fbf38fc662dcc6473a2af88a7
Avantgarde95 <95phm@kaist.ac.kr>
2021-06-06 21:38:26 +0900

    [feat] Viewer animation
    * Put a simple animation to the viewer when changing the article

--- src/view/viewer/Viewer.tsx
+++ src/view/viewer/Viewer.tsx
@@ -9,16 +9,19 @@ export const Viewer = () => {
     const article = articles[articleIndex];
 
     return (
-        <div
-            className={'Viewer'}
-            onClick={() => {
-                openMenu(false);
-            }}
-        >
-            <div className={'ArticleTitle'}>{article.title}</div>
-            {article.sections.map((section, index) => (
-                <SectionView index={index} section={section} />
-            ))}
+        <div className={'Viewer'}>
+            <div
+                className={'Area'}
+                key={article.title} // Force React to re-render Area when the article is changed.
+                onClick={() => {
+                    openMenu(false);
+                }}
+            >
+                <div className={'ArticleTitle'}>{article.title}</div>
+                {article.sections.map((section, index) => (
+                    <SectionView index={index} section={section} />
+                ))}
+            </div>
         </div>
     );
 };
